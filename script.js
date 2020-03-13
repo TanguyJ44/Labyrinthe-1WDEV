75ba25728e488c86f375d86930862ab347e00812
Tanguy-Supinfo <303082@supinfo.com>
2020-03-13 11:20:39 +0100

    keys

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-var roads = [];
+var roads = ["case12", "case22", "case32", "case42", "case44", "case52", 
+            "case53","case54", "case64", "case74", "case83", "case84", 
+            "case85", "case86", "case96", "case106"];
 
 function fieldGenerator () {
-    
-    for (let i = 0; i < 10; i++) {
-    
-        
-
+    for (let i = 0; i < roads.length; i++) {
+        document.getElementById(roads[i]).style.backgroundColor = "#bdc3c7";
     }
-    
 }
+
+document.addEventListener('keydown', (event) => {
+    const nomTouche = event.key;
+  
+    if (nomTouche === 'Control') {
+      return;
+    }
+  
+    if (event.ctrlKey) {
+      
+    } else {
+        if(nomTouche == "ArrowUp") {
+            console.log("UP");
+        } else if(nomTouche == "ArrowDown") {
+            console.log("DOWN");
+        } else if(nomTouche == "ArrowRight") {
+            console.log("RIGHT");
+        } else if(nomTouche == "ArrowLeft") {
+            console.log("LEFT");
+        }
+    }
+}, false);
+
